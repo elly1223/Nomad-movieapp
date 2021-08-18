a038a79a85543a2285a84eddb35b59bb457fbe56
yunkyung Lee <62500947+leeykyk@users.noreply.github.com>
2021-08-18 10:59:19 +0900

    Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
-}
+// class형은 return을 가지지 않는다.
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Food fav='kimchi' />
-      <Food fav='ramen' />
-      <Food fav='samgiopsal' />
-      <Food fav='chukumi' />
-    </div>
-  );
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+
+  componentDidMount() {
+    console.log('Component rendered');
+  }
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
+  componentWillUnmount() {
+    console.log('Goodbye, cruel world');
+  }
+
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
